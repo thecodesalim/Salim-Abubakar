4cc1730cc768fdbf815d36f408fa9da8d8ef691c
Salim Abubakar <samatazu@gmail.com>
2021-01-06 00:04:29 +0100

    Navigation between Filter Screen and Car Screen

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
+import 'react-native-gesture-handler';
 import RNFS from 'react-native-fs';
 import Papa from 'papaparse';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import React from 'react';
 
 import {StyleSheet, Text, View, Pressable, Button} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 
 function Display(props) {
-  const pressed = () => {
-    console.log('pressed');
-  };
   return (
     <View>
       <Text style={styles.header}>Filters</Text>
       {props.filter.map((filter) => (
         <View style={styles.filterCard} key={filter.id}>
           <View styles={styles.filterText}>
-            <Pressable onPress={pressed}>
+            <Pressable onPress={props.onPress}>
               <Text> Start year: {filter.start_year}</Text>
               <Text> End year: {filter.end_year}</Text>
               <Text> Gender:{filter.gender}</Text>
@@ -29,22 +28,36 @@ function Display(props) {
     </View>
   );
 }
-function Data(props) {
-  const [data, setData] = React.useState([]);
 
+function Cars(props) {
   return (
     <View>
+      <Text>Cars</Text>
       <Button
         onPress={props.onPress}
         title="More"
         color="#841584"
         accessibilityLabel="Learn more about this purple button"
       />
+      <ScrollView>
+        {props.filtered.map((filter) => (
+          <View style={styles.carFilter} key={filter.id}>
+            <Text> {`${filter.first_name} ${filter.last_name}`}</Text>
+            <Text> {filter.job_title}</Text>
+            <Text> {filter.email}</Text>
+            <Text> {filter.country}</Text>
+            <Text>
+              {`${filter.car_model} ${filter.car_model_year} ${filter.car_color}`}
+            </Text>
+            <Text> {filter.country}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
-export default function App() {
+function HomeScreen({navigation}) {
   const [data, setData] = React.useState([]);
 
   React.useEffect(() => {
@@ -59,6 +72,15 @@ export default function App() {
     setData(data.concat(filters));
     //console.log(filters[0].countries)
   };
+  return (
+    <View style={styles.container}>
+      <Display filter={data} onPress={() => navigation.navigate('Cars')} />
+    </View>
+  );
+}
+
+function FilterScreen() {
+  const [data, setData] = React.useState([]);
 
   const loadAllCSV = () => {
     var mainBundlePath = RNFS.MainBundlePath;
@@ -78,23 +100,39 @@ export default function App() {
           'Greece',
           'Peru',
         ];
-        console.log(
-          results.data
-            .filter((x) => array.includes(x.country))
-            .filter((x) => x.car_model_year > 1991 && x.car_model_year < 2009)
-            .filter((x) => x.gender.toLowerCase() === 'male'.toLowerCase())
-            .filter((x) =>
-              ['Green', 'Violet', 'Yellow', 'Blue'].includes(x.car_color),
-            ),
+        setData(
+          data.concat(
+            results.data
+              .filter((x) => array.includes(x.country))
+              .filter((x) => x.car_model_year > 1991 && x.car_model_year < 2009)
+              .filter((x) => x.gender.toLowerCase() === 'male'.toLowerCase())
+              .filter((x) =>
+                ['Green', 'Violet', 'Yellow', 'Blue'].includes(x.car_color),
+              ),
+          ),
         );
+        console.log(data);
       },
     });
   };
 
   return (
     <View style={styles.container}>
-      <Data onPress={loadAllCSV} />
+      <Cars filtered={data} onPress={loadAllCSV} />
     </View>
+  );
+}
+
+export default function App() {
+  const Stack = createStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Filters" component={HomeScreen} />
+        <Stack.Screen name="Cars" component={FilterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -102,7 +140,7 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 40,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFFFFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -115,12 +153,22 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     margin: 10,
-    width: 380,
+    width: 350,
     height: 110,
-    backgroundColor: '#d9d5d4',
+    backgroundColor: '#ebebeb',
     borderRadius: 10,
   },
   filterText: {
     paddingTop: 100,
+  },
+  carFilter: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    width: 350,
+    height: 150,
+    backgroundColor: '#ebebeb',
+    borderRadius: 10,
   },
 });
